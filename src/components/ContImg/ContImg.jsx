199017b08a6fde922e83468556132e4e17548d90
DiegoBelPe <dbeltranpereira@gmail.com>
2022-03-10 20:53:02 -0500

    update-folder-ContImg

---
+++ src/components/ContImg/ContImg.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContImg = () => {
+  return (
+    <div>ContImg</div>
+  )
+}
+
+export default ContImg
---
+++ src/components/ContImg/index.js
@@ -0,0 +1,15 @@
+/* import logo from './logo.svg'; */
+import './App.css';
+import ContImg from './ContImg';
+
+function App() {
+  return (
+    <div className="App">
+      <div><ContImg/></div>
+      
+        
+    </div>
+  );
+}
+
+export default App;
