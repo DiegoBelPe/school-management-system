b639c43a3aacdb272b5c3fdb98d3e7c63c379475
borismg04 <borismg04@gmail.com>
2022-03-12 22:03:00 -0500

    feature:footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer/index';
+
+
 
 function App() {
   return (
     <div className="App">
       {/* <div>navBar</div>
       <div>ContImg</div>
-      <div>Cards</div>
-      <div>Footer</div> */}
+      <div>Cards</div>*/}
+      <div><Footer/></div>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Footer";
+import './Footer.css'
+
+function index() {
+  return (
+    <div className="App">
+      {/* <div>navBar</div>
+      <div>ContImg</div>
+      <div>Cards</div>*/}
+      <div className='footer'><Footer/></div> 
+    </div>
+  );
+}
+
+export default index;
