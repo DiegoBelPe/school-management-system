13e960f6cfbea337aff30b76653044a46f9b5f6a
borismg04 <borismg04@gmail.com>
2022-03-12 22:12:15 -0500

    feture:solution mergingv1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import './App.css';
-import Footer from './components/Footer/index';
-
-
-
-function App() {
-  return (
-    <div className="App">
-      {/* <div>navBar</div>
-      <div>ContImg</div>
-      <div>Cards</div>*/}
-      <div><Footer/></div>
-    </div>
-=======
 /* import logo from './logo.svg'; */
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -31,7 +16,6 @@ function App() {
 
     
     </BrowserRouter>
->>>>>>> 990fbedfd906e31b875c503f3788a2154ea3631e
   );
 }
 
