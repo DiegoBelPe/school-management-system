c6cde0aac09328087e60472954c46b5ffa7e980c
jose_miguel <josecastrillong@gmail.com>
2022-03-17 15:55:11 -0500

    feat: week agenda in spanish and personalized display

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import React from 'react';
+import FullCalendar from '@fullcalendar/react'; // must go before plugins
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+
 
 const Schedule = () => {
     return (
-
-        <div>
-              <FullCalendar
-          plugins={[ dayGridPlugin ]}
-          initialView="dayGridMonth"
-        />
-        </div>
-      
-      )
+      <div>
+        <FullCalendar 
+          plugins={[timeGridPlugin]}
+          locale = 'es'
+          weekends = {false}
+          headerToolbar={{
+            left: '',
+            center: '',
+            right: ''
+          }}
+          dayHeaderFormat = {{ weekday: 'long'}}
+          slotMinTime = '6:00'
+          slotMaxTime= '19:00'
+          allDaySlot = {false}
+          slotLabelFormat = {{hour: 'numeric',  minute: '2-digit'}}
+          slotDuration = '1:00:00'
+          expandRows = {true}
+          aspectRatio = '1.5'
+        />  
+      </div>    
+    );
 }
    
 export default Schedule;
