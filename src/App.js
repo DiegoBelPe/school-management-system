7e9587151eba84ae3e5e49013356f21970fa20c1
borismg04 <borismg04@gmail.com>
2022-03-18 16:47:40 -0500

    feat/add component Accordion

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import ContImg from './components/ContImg'
+// import ContImg from './components/ContImg'
 import NavBar from './components/NavBar';
 import Footer from './components/Footer/Footer';
 import Cards from './components/Cards/Cards';
 import Dash from './pages/Dash/Index'
 import DashNav from './components/DashNav/DasNav'
+import Accordion from './components/Accordion';
 
 function App() {
   return (
@@ -15,13 +16,12 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/dash' element={<Dash/>}/>
-       <Route path='/dashnav' element={<DashNav/>}/> 
+      <Route path='/dashnav' element={<DashNav/>}/> 
       <Route path='/navbar' element={<NavBar/>}/>
       <Route path='/footer' element={<Footer/>}/>
       <Route path='/card' element={<Cards/>}/>
+      <Route path='/accordion' element={<Accordion/>}/>
     </Routes>
-
-    
     </BrowserRouter>
   );
 }
