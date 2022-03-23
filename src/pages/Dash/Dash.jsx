03294453c29a44335f5009cb2e246044e982e2aa
DiegoBelPe <dbeltranpereira@gmail.com>
2022-03-23 12:02:36 -0500

    update component dash

--- src/pages/Dash/Dash.jsx
+++ src/pages/Dash/Dash.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import dash from './Dash.module.css'
-import FunctionContainer from '../../components/FunctionContainer/FunctionContainer'
+import React from 'react';
+import dash from './Dash.module.css';
+import DashNav from '../../components/DashNav/DasNav';
+import DataTable from '../../components/DataTable/DataTable';
+import Schedule from '../../components/Schedule/Schedule';
 
-const DashBoard = () => {
+const Dash= () => {
   return (
     <>
-        <div className={dash.grid__container}>
-            <div className={dash.header}>HEADER</div>
-            <div className={dash.navbar}>NAVBAR</div>
-            <div className={dash.main}><FunctionContainer/></div>
-            <div className={dash.footer}>FOOTER</div>
+      <div className={dash.container__dash}>
+        <div className={dash.header__dash}>HEADER</div>
+        <div className={dash.navbar}>
+          <DashNav />
         </div>
+        <div className={dash.main}><Schedule/></div>
+        <div className={dash.footer}>FOOTER</div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default DashBoard;
+export default Dash;
--- src/pages/Dash/index.js
+++ src/pages/Dash/index.js
@@ -1,14 +1,13 @@
-import React from "react";
-import DashBoard from "./Dash";
-import './Dash.module.css'
-
+import React from 'react';
+import DashBoard from './Dash';
+import './Dash.module.css';
 
 function index() {
-    return (
-      <div className="App">
-          <DashBoard/>       
-      </div>
-    );
-  }
-  
-  export default index;
+  return (
+    <div className="App">
+      <DashBoard />
+    </div>
+  );
+}
+
+export default index;
