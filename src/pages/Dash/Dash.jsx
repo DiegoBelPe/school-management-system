692378a1af1613f5d2acc8b954cc663bf427f88f
borismg04 <borismg04@gmail.com>
2022-03-27 17:44:27 -0500

    bug-fix merge1

--- src/pages/Dash/Dash.jsx
+++ src/pages/Dash/Dash.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import styles from './Dash.module.css';
 import DashNav from '../../components/DashNav/DasNav';
-<<<<<<< HEAD
-import DataTable from '../../components/DataTable/DataTable';
-import Schedule from '../../components/Schedule/Schedule';
-import Accordion from '../../components/Accordion/Accordion'
-=======
->>>>>>> 135fb5efae654e12d63bdb62877547a7636648d0
 
 
 const Dash = () => {
@@ -19,16 +13,10 @@ const Dash = () => {
         <div className={styles.navbar}>
           <DashNav />
         </div>
-<<<<<<< HEAD
-        <div className={dash.main}><Schedule/></div>
-        <div className={Accordion.accordion}><Accordion/></div>
-        <div className={dash.footer}>FOOTER</div>
-=======
         <div className={styles.main}>
           <Outlet />
         </div>
         <div className={styles.footer}>FOOTER</div>
->>>>>>> 135fb5efae654e12d63bdb62877547a7636648d0
       </div>
     </>
   );
