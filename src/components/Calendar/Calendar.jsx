8552eddfc9a2e97fea4b7d8ac848c0c3d38e0620
jose_miguel <josecastrillong@gmail.com>
2022-03-27 22:58:42 -0500

    feat: created HTTP helpers

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -3,11 +3,20 @@ import Modals from '../../pages/Modals'
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import useSWR from 'swr';
 
 
 
-
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 const Calendar = () => {
+    const { data: events, error } = useSWR('http://localhost:8080/api/events', fetcher);
+    if(error) {
+      return <div>Tenemos un problema...</div>
+    }
+    if(!events) {
+      return <div>Cargando...</div>
+    }
+
     return (
         <div>
             <Modals></Modals>
