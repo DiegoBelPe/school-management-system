399f4fe65a854556ad83b9b259999f57ec8d1325
DiegoBelPe <dbeltranpereira@gmail.com>
2022-04-27 10:47:49 -0500

    update paymosn

--- src/components/Payments/Payments.jsx
+++ src/components/Payments/Payments.jsx
@@ -3,7 +3,9 @@ import { Elements } from '@stripe/react-stripe-js';
 
 import CheckoutForm from './CheckoutForm';
 
-const stripePromise = loadStripe('pk_test_51KsUIaBULQhuEZz1PVPpwq9jmGIedKsRxQmy2LjCSU991vNgJ24CzxQHu8WtLdMbnbXNeYhfx0pk8fYgU9SdJTnY009RQ9ENBh');
+const stripePromise = loadStripe(
+  'pk_test_51KsUIaBULQhuEZz1PVPpwq9jmGIedKsRxQmy2LjCSU991vNgJ24CzxQHu8WtLdMbnbXNeYhfx0pk8fYgU9SdJTnY009RQ9ENBh',
+);
 
 function Payments() {
   return (
