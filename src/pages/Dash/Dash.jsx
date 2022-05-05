893e9c2ab37cd1cc44d8f83d2d61178c4c36a7d1
DiegoBelPe <dbeltranpereira@gmail.com>
2022-05-05 11:07:48 -0500

    update view navDash en backgroung

--- src/pages/Dash/Dash.jsx
+++ src/pages/Dash/Dash.jsx
@@ -6,14 +6,14 @@ import DashNav from '../../components/DashNav/DasNav';
 function Dash() {
   return (
     <div className={styles.container__dash}>
-      <div className={styles.header__dash}>HEADER</div>
+      {/* <div className={styles.header__dash}>HEADER</div> */}
       <div className={styles.navbar}>
         <DashNav />
       </div>
       <div className={styles.main}>
         <Outlet />
       </div>
-      <div className={styles.footer}>FOOTER</div>
+      {/* <div className={styles.footer}>FOOTER</div> */}
     </div>
   );
 }
